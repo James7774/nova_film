664036ba857d3b5f95ed07eea091169fbd820bed
James7774 <james7774@example.com>
2026-04-16 14:52:01 +0500

    Removed admin ID 6301282347

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-ADMINS = [int(admin_id) for admin_id in os.getenv("ADMINS", "12345678,6301282347").split(",") if admin_id.strip()]
+ADMINS = [int(admin_id) for admin_id in os.getenv("ADMINS", "12345678").split(",") if admin_id.strip()]
 CHANNELS = [channel.strip() for channel in os.getenv("CHANNELS", "@novaxrasmiy").split(",") if channel.strip()]
 INSTAGRAM_LINK = "https://www.instagram.com/nova_kino.uz/"
 STORAGE_CHANNEL_ID = os.getenv("STORAGE_CHANNEL_ID", "@nova_storage") # Replace with your channel ID or username
